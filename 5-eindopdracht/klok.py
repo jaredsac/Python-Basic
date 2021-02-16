2c81b32d3eafcb73ca6761ac438adcda5f453bc6
Jared Sackey <jared.sackey@student.rocva.nl>
2021-02-16 15:27:41 +0100

    bijnna

--- 5-eindopdracht/klok.py
+++ 5-eindopdracht/klok.py
@@ -1,18 +1,19 @@
+from turtle import *
+import time
 seconde = 58
 minuten = 59
 uren = 23
 
-import time
 
-from turtle import *
 setup()
 tl = Turtle()
 
 while True:
-    tl.clear()# met deze functie gebruik ik om het getalen niet over elkaat te schrijven.
-    tl.write(str(uren).zfill(2) + ":" + str(minuten).zfill(2)  + ":" + str(seconde).zfill(2),
-             font= ("times new roman", 25, "normal"))
-    #hier dwing python om twee getalen te gebruiken.
+    # met deze functie gebruik ik om het getalen niet over elkaat te schrijven.
+    tl.clear()
+    tl.write(str(uren).zfill(2) + ":" + str(minuten).zfill(2) + ":" + str(seconde).zfill(2),
+             font=("times new roman", 25, "normal"))
+    # hier dwing python om twee getalen te gebruiken.
 
     seconde = seconde + 1
     time.sleep(1)
@@ -20,13 +21,13 @@ while True:
     if seconde == 60:
         seconde = 0
         minuten = minuten + 1
-    #hier laat ik zien dat als seconde op 60 spring dan komt er een minuut bij.
+    # hier laat ik zien dat als seconde op 60 spring dan komt er een minuut bij.
     #
     if minuten == 60:
         minuten = 0
         uren = uren + 1
-    #nu doet de functie als het 60 minuten bereikt dan komt er 1 bij de de uren.
-    # 
+    # nu doet de functie als het 60 minuten bereikt dan komt er 1 bij de de uren.
+    #
     if uren == 24:
         uren = 0
-    #hier doe ik dus als de uren op 24 springt dat show het 00 in plaats van 24.
+    # hier doe ik dus als de uren op 24 springt dat show het 00 in plaats van 24.
